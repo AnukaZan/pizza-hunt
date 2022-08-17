ac2931fab2541e76b9f06272da8ac53d465d9af8
Anuka <anukazan@gmail.com>
2022-08-16 18:01:23 -0700

    edit server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,17 +7,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use(require('./routes'));
 
-//tell mongoose which database to connect to
-//if MOGODB_URI exists, use that. Otherwise, use local server
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+  useUnifiedTopology: true 
+}); 
 
-//log mongo queries being executed
+// Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
